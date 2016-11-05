78725d17d6b2e83de15505fd26fcc8d1662fb361
Stephen Narwold <stevish@gmail.com>
2016-11-05 15:23:52 -0400

    Add more variables to the admin forms

--- form-admin.php
+++ form-admin.php
@@ -16,9 +16,17 @@
         <input type="password" name="pass1" id="pass1" /></p>
     <p><label for="pass2">New Password again (optional):</label><br/>
         <input type="password" name="pass2" id="pass2" /></p>
-    <p><label for="ssid">SSID (Name of the Wi-Fi Network.)</label><br/>
-        <input type="text" name="ssid" id="ssid" value="<?php echo $admin->config['ssid']; ?>" /></p>
-    <input type="submit" value="Run Setup" />
+	<p><label for="ssid">SSID (Name of the Wi-Fi Network.)</label><br/>
+		<input type="text" name="ssid" id="ssid" value="<?php echo $admin->config['ssid']; ?>" /></p>
+	<p><label for="channel">Wi-Fi Channel:</label><br/>
+		<input type="text" name="channel" id="channel" value="<?php echo $admin->config['channel']; ?>" /></p>
+	<p><label for="hostname">Host Name (The Domain users will use, like biblebox.lan):</label><br/>
+		<input type="text" name="hostname" id="hostname" value="<?php echo $admin->config['hostname']; ?>" /></p>
+	<p><label for="txpower">Tx Power</label><br/>
+		<input type="text" name="txpower" id="txpower" value="<?php echo $admin->config['txpower']; ?>" /></p>
+	<p><label for="system_hostname">System Hostname</label><br/>
+		<input type="text" name="system_hostname" id="system_hostname" value="<?php echo $admin->config['system_hostname']; ?>" /></p>
+    <input type="submit" value="Update" />
 </form>
 <form method="POST">
     <input type="hidden" name="action" value="unmount" />
