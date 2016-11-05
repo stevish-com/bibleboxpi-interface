0d1431f3908ce800ab5f970ed450c3e139965f09
Stephen Narwold <stevish@gmail.com>
2016-11-05 09:42:33 -0400

    Second Inital Commit :)

--- admin.php
+++ admin.php
@@ -1,24 +1,40 @@
 <html><head></head><body>
 <?php
+//Get the config variable from the single config file
+//Config should include SSID and general options
+//Doesn't necessarily need to store homepage messages, as those will be in index.html
 $config = unserialize( file_get_contents ('config') );
+authenticate();
+
 if ( ! $config ) {
     if ( 'setup' == $_POST['action'] ) {
-
+        //Do setup (create config variable, dump it to config file)
     } else {
         include( 'form-setup.php' );
     }
 
-<?php } else { ?>
-    Please enter your password.
-    <form method="POST">
-        <input type="password" name="password" />
-        <input type="submit" value="Log In">
-    </form>
-
-<?php }
-if ( ! isset ( $_POST['pass'] ) ) {
+} else {
+    if ( ! isset ( $_POST['pass'] ) ) {
+        header('Location: http://biblebox.com/login.html');
+        die();
+    } elseif ( bbpi_hash($_POST['pass']) != $config['hash'] ) {
+        // TODO: Brute Force Limiter. Should be done in a way that can be reset manually by plugging the USB stick into a computer
+        header('Location: http://biblebox.com/login-fail.html');
+        die();
+    } else {
+        include( 'form-admin.php' );
+    }
 
 }
+
 ?>
 </body>
 </html>
+
+
+<?php
+
+function authenticate() {
+    session_start();
+    
+}
---
+++ form-admin.php
@@ -0,0 +1,10 @@
+<p>Manage your settings below:</p>
+<form method="POST">
+    <p><label for="pass1">New Password (optional):</label><br/>
+        <input type="password" name="pass1" id=pass1" /></p>
+    <p><label for="pass2">New Password again (optional):</label><br/>
+        <input type="password" name="pass2" /></p>
+    <p><label for="ssid">SSID (Name of the Wi-Fi Network)</label><br/>
+        <input type="text" name="ssid" /></p>
+    <input type="submit" value="Run Setup" />
+</form>
