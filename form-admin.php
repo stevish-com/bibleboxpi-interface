792209dbebbe7c17b055a7cc109f55bd1dda04e3
Stephen Narwold <stevish@gmail.com>
2016-11-05 12:58:36 -0400

    Fix some HTML for the labels

--- form-admin.php
+++ form-admin.php
@@ -10,11 +10,11 @@
 <form method="POST">
     <input type="hidden" name="action" value="admin" />
     <p><label for="pass1">New Password (optional):</label><br/>
-        <input type="password" name="pass1" id=pass1" /></p>
+        <input type="password" name="pass1" id="pass1" /></p>
     <p><label for="pass2">New Password again (optional):</label><br/>
-        <input type="password" name="pass2" /></p>
+        <input type="password" name="pass2" id="pass2" /></p>
     <p><label for="ssid">SSID (Name of the Wi-Fi Network.)</label><br/>
-        <input type="text" name="ssid" value="<?php echo $admin->config['ssid']; ?>" /></p>
+        <input type="text" name="ssid" id="ssid" value="<?php echo $admin->config['ssid']; ?>" /></p>
     <input type="submit" value="Run Setup" />
 </form>
 <form method="POST">
--- form-setup.php
+++ form-setup.php
@@ -10,11 +10,11 @@
 <form method="POST">
     <input type="hidden" name="action" value="setup" /> 
     <p><label for="pass1">Password:</label><br/>
-        <input type="password" name="pass1" /></p>
+        <input type="password" name="pass1" id="pass1" /></p>
     <p><label for="pass2">Password again:</label><br/>
-        <input type="password" name="pass2" /></p>
+        <input type="password" name="pass2" id="pass2" /></p>
     <p><label for="ssid">SSID (Name of the Wi-Fi Network)</label><br/>
-        <input type="text" name="ssid" /></p>
+        <input type="text" name="ssid" id="ssid" /></p>
     <input type="submit" value="Run Setup" />
 </form>
 </body>
